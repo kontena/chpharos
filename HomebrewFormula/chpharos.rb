a5d85641034e3d915b58b8e28826d8388f650e9b
Kimmo Lehto <kimmo.lehto@gmail.com>
2018-07-30 14:10:38 +0300

    Add an osx homebrew formula (#14)

---
+++ HomebrewFormula/chpharos.rb
@@ -0,0 +1,25 @@
+class Chpharos < Formula
+  desc "Kontena Pharos tool chain version installer and switcher"
+  homepage "https://github.com/kontena/chpharos#readme"
+  url "https://github.com/kontena/chpharos/archive/v0.1.0.tar.gz"
+  sha256 "01bc786af2d2e7810add0326b2ca8de74cb77c9db44eb5329b00bcb81fc3f642"
+  head "https://github.com/kontena/chpharos.git"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  def caveats; <<~EOS
+    Add the following to the ~/.bash_profile or ~/.zshrc file:
+      source #{opt_pkgshare}/chpharos.sh
+
+    To enable auto-switching of Pharos versions specified by .pharos-version files,
+    add the following to ~/.bash_profile or ~/.zshrc:
+      chpharos auto
+  EOS
+  end
+
+  test do
+    assert_equal "chpharos #{version}", shell_output("#{bin}/chpharos-exec --version").strip
+  end
+end
